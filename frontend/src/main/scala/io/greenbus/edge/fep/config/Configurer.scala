9ec844f0e82da7b512472283933307d7c54c5026
devans <devans@greenenergycorp.com>
2017-04-24 00:47:51 -0400

    Configuration handle work

--- frontend/src/main/scala/io/greenbus/edge/fep/config/Configurer.scala
+++ frontend/src/main/scala/io/greenbus/edge/fep/config/Configurer.scala
@@ -18,8 +18,12 @@
  */
 package io.greenbus.edge.fep.config
 
-import io.greenbus.edge.api.stream.ProducerHandle
-import io.greenbus.edge.api.{ EndpointId, Path }
+import java.util.UUID
+
+import io.greenbus.edge.api.stream.{ EndpointBuilder, ProducerHandle }
+import io.greenbus.edge.api._
+import io.greenbus.edge.data.ValueString
+import io.greenbus.edge.flow
 import io.greenbus.edge.peer.{ AmqpEdgeService, ProducerServices }
 import io.greenbus.edge.thread.{ CallMarshaller, EventThreadService }
 
@@ -35,6 +39,8 @@ object Configurer {
 
     val eventThread = EventThreadService.build("Config")
 
+    val end = FepConfigureEndpoint.load(eventThread, EndpointId(Path(Seq("fep", "config", "dnp3"))), producerServices)
+
     System.in.read()
 
     //gatewayMgr.close()
@@ -43,21 +49,44 @@ object Configurer {
   }
 }
 
+class FepConfigureHandle(b: EndpointBuilder) {
+
+  val dnpHandle = b.activeSet(Path("dnp3"))
+
+  val uploadHandle = b.outputStatus(Path(Seq("dnp3", "upload")))
+  val req = b.registerOutput(Path(Seq("dnp3", "upload")))
+}
+
 object FepConfigureEndpoint {
   def load(eventThread: CallMarshaller, id: EndpointId, producerServices: ProducerServices): FepConfigureEndpoint = {
 
     val b = producerServices.endpointBuilder(id)
 
-    val dnpHandle = b.activeSet(Path("dnp3"))
-
-    val uploadHandle = b.outputStatus(Path(Seq("dnp3", "upload")))
-    b.registerOutput(Path(Seq("dnp3", "upload")))
+    val cfg = new FepConfigureHandle(b)
 
     val handle: ProducerHandle = b.build(100, 100)
 
-    ???
+    new FepConfigureEndpoint(eventThread, handle, cfg)
   }
 }
-class FepConfigureEndpoint(eventThread: CallMarshaller, handle: ProducerHandle) {
+class FepConfigureEndpoint(eventThread: CallMarshaller, handle: ProducerHandle, end: FepConfigureHandle) {
+
+  private val session = UUID.randomUUID()
+  eventThread.marshal {
+    end.dnpHandle.update(Map(ValueString("name01") -> ValueString("temp"), ValueString("name02") -> ValueString("temp")))
+    end.uploadHandle.update(OutputKeyStatus(session, 0, None))
+  }
+
+  end.req.bind(new flow.Responder[OutputParams, OutputResult] {
+    def handle(obj: OutputParams, respond: (OutputResult) => Unit): Unit = {
+      eventThread.marshal {
+        onUploadReq(obj, respond)
+      }
+    }
+  })
+
+  def onUploadReq(params: OutputParams, respond: OutputResult => Unit): Unit = {
+    println(params)
+  }
 
 }
