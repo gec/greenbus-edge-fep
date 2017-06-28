bfe07fa1926b5fc9b7b61d4bbdb1f0defae7f209
devans <devans@greenenergycorp.com>
2017-06-28 18:38:56 -0400

    Temp work on dyn config

--- configure-db/src/main/scala/io/greenbus/edge/configure/dyn/ConfigurationTable.scala
+++ configure-db/src/main/scala/io/greenbus/edge/configure/dyn/ConfigurationTable.scala
@@ -1,12 +1,34 @@
+/**
+ * Copyright 2011-2017 Green Energy Corp.
+ *
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. Green Energy
+ * Corp licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.greenbus.edge.configure.dyn
 
 import io.greenbus.edge.api._
-import io.greenbus.edge.configure.endpoint.{ModuleConfiguration, ModuleConfigurer}
+import io.greenbus.edge.configure.endpoint.{ FepConfigurerMgr, ModuleConfiguration, ModuleConfigurer }
+import io.greenbus.edge.configure.sql.{ ModuleComponentValue, ModuleDb }
+import io.greenbus.edge.data.{ IndexableValue, Value, ValueString }
+import io.greenbus.edge.data.proto.convert.ValueConversions
 import io.greenbus.edge.thread.CallMarshaller
+
 import scala.collection.mutable
+import scala.concurrent.{ Future, Promise }
 
-import scala.concurrent.Promise
-
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /*
 
@@ -20,9 +42,13 @@ c) subscribe to component/node for modules, active set is module -> hash, then h
 
  */
 
-class ConfigurationTable(eventThread: CallMarshaller, endpointId: EndpointId, producerService: ProducerService) extends ModuleConfigurer {
+class ConfigurationTable(eventThread: CallMarshaller, db: ModuleDb, endpointId: EndpointId, producerService: ProducerService) extends ModuleConfigurer {
 
-  //private val subscribedMap = mutable.Map.empty[]
+  // component -> (module -> value)
+  private val subscribedMap = mutable.Map.empty[String, ActiveSetHandle]
+
+  private var dynSetHandleOpt = Option.empty[DynamicActiveSetHandle]
+  private var producerHandleOpt = Option.empty[ProducerHandle]
 
   eventThread.marshal {
     init()
@@ -31,7 +57,7 @@ class ConfigurationTable(eventThread: CallMarshaller, endpointId: EndpointId, pr
   private def init(): Unit = {
     val builder = producerService.endpointBuilder(endpointId)
 
-    val setHandle = builder.keyValueDynamicSet("configuration", new DynamicDataKey {
+    val setHandle = builder.activeSetDynamicSet("configuration", new DynamicDataKey {
       def subscribed(path: Path): Unit = {
         eventThread.marshal(onSubscribed(path))
       }
@@ -41,22 +67,44 @@ class ConfigurationTable(eventThread: CallMarshaller, endpointId: EndpointId, pr
       }
     })
 
-    val endpointHandle = builder.build()
+    dynSetHandleOpt = Some(setHandle)
+    producerHandleOpt = Some(builder.build())
   }
 
   private def onSubscribed(path: Path): Unit = {
-
+    val componentName = path.parts.mkString("/")
+    db.valuesForComponent(componentName).foreach { values =>
+      eventThread.marshal {
+        dynSetHandleOpt.foreach { h =>
+          val handle: ActiveSetHandle = h.add(path)
+          val activeSet: Map[IndexableValue, Value] = values.flatMap { mv =>
+            FepConfigurerMgr.fromDbBytes(s"${mv.module}/${mv.component}", mv.data)
+              .map(v => (ValueString(mv.module), v))
+          }.toMap
+          handle.update(activeSet)
+        }
+      }
+    }
   }
 
   private def onUnsubscribed(path: Path): Unit = {
-
+    dynSetHandleOpt.foreach { h => h.remove(path) }
   }
 
   def updateModule(module: String, config: ModuleConfiguration, promise: Promise[Boolean]): Unit = {
-
+    val updates = config.components.map { case (comp, v) => ModuleComponentValue(module, comp, ValueConversions.toProto(v).toByteArray) }
+    val futs = updates.map(v => db.insertValues(v))
+    Future.sequence(futs).foreach { _ =>
+      eventThread.marshal {
+        onModuleUpdate(module, config, promise)
+      }
+    }
   }
 
   def removeModule(module: String, promise: Promise[Boolean]): Unit = {
 
+  }
+
+  private def onModuleUpdate(module: String, config: ModuleConfiguration, promise: Promise[Boolean]): Unit = {
   }
 }
